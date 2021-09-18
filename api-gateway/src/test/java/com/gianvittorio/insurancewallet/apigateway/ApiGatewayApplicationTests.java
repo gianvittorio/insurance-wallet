7640069cff8a8950f808698263cc559d64f90cf3
gianvittorio <gianvittorio.castellano@gmail.com>
2021-09-18 20:26:14 -0300

    introduce common, discovery service and api gateway

---
+++ api-gateway/src/test/java/com/gianvittorio/insurancewallet/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gianvittorio.insurancewallet.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
