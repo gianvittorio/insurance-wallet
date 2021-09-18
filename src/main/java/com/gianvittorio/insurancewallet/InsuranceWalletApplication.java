3a6592bbb47c513e159f37ba1b8f97ba25f44f17
gianvittorio <gianvittorio.castellano@gmail.com>
2021-09-18 16:50:00 -0300

    create project

---
+++ src/main/java/com/gianvittorio/insurancewallet/InsuranceWalletApplication.java
@@ -0,0 +1,13 @@
+package com.gianvittorio.insurancewallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceWalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gianvittorio/insurancewallet/InsuranceWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gianvittorio.insurancewallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
