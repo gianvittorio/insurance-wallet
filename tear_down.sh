86b891c28f7ebd0ee30ed2f0bbdd793271094904
gianvittorio <gianvittorio.castellano@gmail.com>
2021-09-19 20:55:01 -0300

    introduce users service and db

--- tear_down.sh
+++ tear_down.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker-compose down && docker volume rm -f $(docker volume ls --filter name=insurance-wallet)
+docker-compose down && docker volume rm -f $(docker volume ls --filter name=insurance-wallet) && \
+  mvn clean
